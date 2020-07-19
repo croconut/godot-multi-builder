e0209d14553333182c95e6c40316f4f4cd9f14c7
croconut <marsteve@ucsc.edu>
2020-07-18 22:53:37 -0700

    checking if godot presets is quote sensitive

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,8 +30,9 @@ for i in "${strarr[@]}"
 do
     # need to remove spaces/quotes/slashes for filename
     removed_quotes=$(echo "$i" | tr -d \'\"\\\/ | tr -d '\040\011\012\015')
+    $i=$(echo "$i" | tr -d \'\")
     # doesnt matter if $2 has a trailing slash already
-    godot --$export_mode ${i} builds/$removed_quotes-build.zip --path ./$2 -v
+    godot --$export_mode "${i}" builds/$removed_quotes-build.zip --path ./$2 -v
     # debug command
     # touch builds/$removed_quotes-build.zip
 done
